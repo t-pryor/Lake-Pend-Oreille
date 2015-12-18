1569d91a8a4c1c3a70af963983ec214b7f0a5068
Timothy Pryor <tdpryor@gmail.com>
2015-12-17 20:44:18 -0500

    Initial Commit

---
+++ Lake Pend Oreille/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lake Pend Oreille
+//
+//  Created by Tim Pryor on 2015-12-17.
+//  Copyright © 2015 Tim Pryor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lake Pend OreilleTests/Lake_Pend_OreilleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lake_Pend_OreilleTests.swift
+//  Lake Pend OreilleTests
+//
+//  Created by Tim Pryor on 2015-12-17.
+//  Copyright © 2015 Tim Pryor. All rights reserved.
+//
+
+import XCTest
+@testable import Lake_Pend_Oreille
+
+class Lake_Pend_OreilleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
